f8c80dabceac8ed24d251e1d1cc716efeb753f9d
danwatt <danwatt@gmail.com>
2013-06-22 22:54:35 -0500

    Stubbing
    Moving
    
    More stubbing
    
    Stubbing out the application
    
    Stubbing
    
    Index
    
    Stubbing a very basic one-page UI
    
    GitKeep
    
    Adding the JS libraries that I want to use

---
+++ src/main/java/org/danwatt/esourceshare/controller/SourceController.java
@@ -0,0 +1,14 @@
+package org.danwatt.esourceshare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SourceController {
+
+	@RequestMapping(value="",method=RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+}
