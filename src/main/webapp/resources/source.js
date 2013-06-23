00890e70f89848cc5a7dcf570e327a0d0b2465f9
danwatt <danwatt@gmail.com>
2013-06-23 15:54:23 -0500

    Resetting repo, stubbed out application

---
+++ src/main/webapp/resources/source.js
@@ -0,0 +1,11 @@
+// Class to represent a row in the seat reservations grid
+function Source() {
+    var self = this;
+}
+
+// Overall viewmodel for this screen, along with initial state
+function SourceShareViewModel() {
+    var self = this;
+}
+
+ko.applyBindings(new SourceShareViewModel());
